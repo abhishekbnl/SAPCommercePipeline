2e124344c0de9b78f77afee2ea4a663ec707eff5
snehamishra <sneha.mishra@bestandless.com.au>
2023-09-10 18:57:39 +1000

    Revert "Update pipelineBuildEveryDay.groovy"

--- pipelines/pipelineBuildEveryDay.groovy
+++ pipelines/pipelineBuildEveryDay.groovy
@@ -28,7 +28,7 @@ pipeline {
                 }
                 cleanWs()
                 checkoutRepository("${projectDir}", "${params.PROJECT_TAG}", "${params.PROJECT_REPO}")
-                //extractCommerce(projectDir)
+                extractCommerce(projectDir)
             }
         }
 
