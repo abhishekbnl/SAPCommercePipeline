588ec6f3d179d5b7b008b90c7fbe78a2d99b736b
Abhishek Singhai <abhishek.singhai@bestandless.com.au>
2024-09-09 10:19:40 +0530

    removed junit as well

--- pipelines/pipelineBuildEveryDayProduction.groovy
+++ pipelines/pipelineBuildEveryDayProduction.groovy
@@ -19,7 +19,7 @@ pipeline {
     options {
         skipDefaultCheckout(true) // No more 'Declarative: Checkout' stage
     }
-    
+
     stages {
         stage('Prepare') {
             steps {
@@ -55,9 +55,9 @@ pipeline {
     }
 
     // post build actions
-    post {
-        always {
-            junit "${relativeJunitLogsPath}/*.xml"
-        }
-    }
+    // post {
+    //     always {
+    //         junit "${relativeJunitLogsPath}/*.xml"
+    //     }
+    // }
 }
